2a271d828a8cda235c2867bfa48e2aaeeb1bc644
gus07ven <gus07ven@users.noreply.github.com>
2016-09-19 23:35:33 -0500

    Maven project initial commit

---
+++ WebProgramming/assignment2/dao/src/main/java/com/myschool/dao/Campus.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myschool.dao;
+
+/**
+ *
+ * @author gustavo
+ */
+public class Campus {
+    
+    private String name;
+    private String location;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    
+}
---
+++ WebProgramming/assignment2/service/src/main/java/com/myschool/service/HCCS.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myschool.service;
+
+import com.myschool.dao.Campus;
+import java.util.List;
+
+/**
+ *
+ * @author gustavo
+ */
+public class HCCS {
+    
+    private List <Campus> campLis;
+}
