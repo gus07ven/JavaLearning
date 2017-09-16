137947405f0307f3ec779b9c7eab5eac28e86814
gus07ven <gus07ven@users.noreply.github.com>
2017-09-16 17:05:56 -0500

    Update program to accept other inputs

--- HW1SD/src/MyList.java
+++ HW1SD/src/MyList.java
@@ -1,14 +1,15 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MyList {
 
-    public int getKey(){
+    public double getKey(){
         Scanner scanner = new Scanner(System.in);
-        int key = Integer.parseInt(scanner.next());
+        double key = Double.parseDouble(scanner.next());
         return key;
     }
 
-    public int binSearch(int[] array, int value) {
+    public String binSearch(Double[] array, double value) {
 
         // Source: majority of algorithm comes from Starting out with Java from Control Structures
         // to Data Structures by Tony Gaddis and Godfrey Muganda. I added some code to adapt it to my need.
@@ -26,31 +27,64 @@ public class MyList {
         while (!found && first <= last) {
             middle = (first + last) / 2;
 
-            if (array[middle] == value) {
-                found = true;
-                position = middle;
-            } else if (array[middle] > value)
+            if (array[middle].doubleValue() == value) {
+                if (middle == 0 || array[middle].doubleValue() != array[middle -1].doubleValue()) {
+                    found = true;
+                    position = middle;
+                } else {
+                    while(array[middle].doubleValue() == array[middle -1].doubleValue()){
+                        middle = middle - 1;
+                    }
+                    found = true;
+                    position = middle;
+                }
+            } else if (array[middle].doubleValue() > value)
                 last = middle - 1;
             else
                 first = middle + 1;
         }
 
         if(position < 0)
-            return 0;
+            return "The key was not found!";
         else
-            return 1;
+            return Integer.toString(position);
+    }
+
+    // I made some modifications and here's the bubbleSort
+    // source: https://www.javatpoint.com/bubble-sort-in-java
+    public Double[] bubbleSort(Double[] inputs){
+
+        int arrLength = inputs.length;
+        double temp;
+
+        // Compare first element with each element in the array
+        for(int i = 0; i < arrLength; i++){
+            for(int j = 1; j < (arrLength - i); j++){
+                // If first element is bigger than element to its right swap
+                if(inputs[j - 1] > inputs[j]){
+                    temp = inputs[j - 1];
+                    inputs[j - 1] = inputs[j];
+                    inputs[j] = temp;
+                }
+            }
+        }
+        return inputs;
     }
 
 
     public static void main(String args[]){
 
-        int[] list = new int[args.length];
+        Double[] inputs = new Double[args.length];
 
         for (int i = 0; i < args.length; i++) {
-                list[i] = Integer.parseInt(args[i]);
+                inputs[i] = Double.parseDouble(args[i]);
         }
 
         MyList myList = new MyList();
-        System.out.println(myList.binSearch(list, myList.getKey()));
+        inputs = myList.bubbleSort(inputs);
+        for(Double input : inputs){
+            System.out.println(input);
+        }
+        System.out.println(myList.binSearch(inputs, myList.getKey()));
     }
 }
--- HW1SD/test/MyListTest.java
+++ HW1SD/test/MyListTest.java
@@ -1,32 +1,57 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
 
-class MyListTest {
+import static org.junit.Assert.*;
+
+public class MyListTest {
 
     @Test
-    void binSearch_KeyFoundInArray_ShouldReturnOne(){
+    public void binSearch_KeyFoundInArray_ShouldReturnOne() throws Exception {
 
-        int[] inputs = {1,2,3,4,5,7,8};
-        int key = 5;
-        int result;
+        Double[] inputs = {1.0, 2.0, 2.0, 4.0, 5.0};
+        Double key = 2.0;
+        String result;
         MyList myList = new MyList();
 
         result = myList.binSearch(inputs, key);
 
-        assertEquals(1, result);
+        assertEquals("1", result);
     }
 
     @Test
-    void binSearch_KeyNotFoundInArray_ShouldReturnZero(){
+    public void binSearch_KeyNotFoundInArray_ShouldReturnZero() throws Exception {
 
-        int[] inputs = {1,2,3,4,5,7,8};
-        int key = 6;
-        int result;
+        Double[] inputs = {1.0, 2.0, 3.0, 4.0, 5.0, 7.0, 8.0};
+        Double key = 6.0;
+        String result;
         MyList myList = new MyList();
 
         result = myList.binSearch(inputs, key);
 
-        assertEquals(0, result);
+        assertEquals("The key was not found!", result);
+    }
+    @Test
+    public void bubbleSort_CorrectlySortInputs_ShouldReturnSortedArray() throws Exception {
+
+        Double[] inputs = {2.5, 1.0, 4.0, 0.5, 2.5, 3.0, 8.0};
+        MyList myList = new MyList();
+        Double first = 0.5;
+        Double second = 1.0;
+        Double third = 2.5;
+        Double fourth = 2.5;
+        Double fifth = 3.0;
+        Double sixth = 4.0;
+        Double seventh = 8.0;
+
+        inputs = myList.bubbleSort(inputs);
+
+        assertEquals(first, inputs[0]);
+        assertEquals(second, inputs[1]);
+        assertEquals(third, inputs[2]);
+        assertEquals(fourth, inputs[3]);
+        assertEquals(fifth, inputs[4]);
+        assertEquals(sixth, inputs[5]);
+        assertEquals(seventh, inputs[6]);
     }
 }
